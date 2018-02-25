2157a7510b9f70b6dd656564979b01684d11dc07
Dima Yarmolchuk <yarmolchuk@gmail.com>
2018-02-25 09:57:26 +0200

    init commit

---
+++ MotoGP/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MotoGP
+//
+//  Created by Dima Yarmolchuk on 2/25/18.
+//  Copyright © 2018 Dima Yarmolchuk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MotoGP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MotoGP
+//
+//  Created by Dima Yarmolchuk on 2/25/18.
+//  Copyright © 2018 Dima Yarmolchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
